db5ff5850a21f9b7351473eb4492ef415eda4627
0xtusharganotra <ganotra.vox@gmail.com>
2025-08-30 18:01:38 +0530

    First commit

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,14 @@
+interface buttonProps {
+  variant: "primary" | "secondary";
+  size: "sm" | "md" | "lg";
+  text: String;
+  startIcon: any;
+  endIcon: any;
+  onClick: () => void;
+}
+
+function Button() {
+  return <></>;
+}
+
+export default Button;
