62e6958499fba2b61a6e35b9d80ddf090251c1b8
0xtusharganotra <ganotra.vox@gmail.com>
2025-09-27 10:16:45 +0530

    decouple dashboard and sidebar from main app file

--- src/components/SidebarList.tsx
+++ src/components/SidebarList.tsx
@@ -1,17 +1,21 @@
+import { LinkIcon } from "../icons/LinkIcon";
+import { NotesIcon } from "../icons/NotesIcon";
+import { TwitterIcon } from "../icons/TwitterIcon";
+import { VideoIcon } from "../icons/YtVideoIcon";
 function SidebarList() {
   return (
     <div className="w-full flex flex-col items-center gap-3 mt-5">
-      <button className="w-[90%] text-white bg-[#181818] shadow-2xl/70 rounded-xl flex justify-start py-2 px-5 cursor-pointer hover:bg-[#2d2d2d]">
-        Tweets
+      <button className="w-[90%] text-white bg-[#181818] shadow-2xl/70 rounded-xl flex justify-start items-center py-2 px-5 cursor-pointer hover:bg-[#2d2d2d] gap-2">
+        <TwitterIcon /> Tweets
       </button>
-      <button className="w-[90%] text-white bg-[#181818] shadow-2xl/70 rounded-xl flex justify-start py-2 px-5 cursor-pointer hover:bg-[#2d2d2d]">
-        Links
+      <button className="w-[90%] text-white bg-[#181818] shadow-2xl/70 rounded-xl flex justify-start items-center gap-2 py-2 px-5 cursor-pointer hover:bg-[#2d2d2d]">
+        <LinkIcon /> Links
       </button>
-      <button className="w-[90%] text-white bg-[#181818] shadow-2xl/70 rounded-xl flex justify-start py-2 px-5 cursor-pointer hover:bg-[#2d2d2d]">
-        Notes
+      <button className="w-[90%] text-white bg-[#181818] shadow-2xl/70 rounded-xl flex justify-start items-center gap-2 py-2 px-5 cursor-pointer hover:bg-[#2d2d2d]">
+        <NotesIcon /> Notes
       </button>
-      <button className="w-[90%] text-white bg-[#181818] shadow-2xl/70 rounded-xl flex justify-start py-2 px-5 cursor-pointer hover:bg-[#2d2d2d]">
-        Videos
+      <button className="w-[90%] text-white bg-[#181818] shadow-2xl/70 rounded-xl flex justify-start items-center gap-1 py-2 px-5 cursor-pointer hover:bg-[#2d2d2d]">
+        <VideoIcon /> Videos
       </button>
     </div>
   );
---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,44 @@
+import Card from "../components/Card";
+import Header from "../components/header";
+import CreateContentDialogue from "../components/createContentDialogue";
+import { useState } from "react";
+function Dashboard() {
+  const [createopen, setcreateopen] = useState(false);
+  return (
+    <div className="w-[80%]">
+      <CreateContentDialogue
+        createopen={createopen}
+        setcreateopen={setcreateopen}
+      />
+      <Header setcreateopen={setcreateopen} />
+      <div className="flex flex-wrap pl-4 justify-start w-[100%] h-[calc(100vh-60px)] pt-8 pb-4 overflow-y-scroll overflow-x-hidden ">
+        <Card
+          title="Titter link"
+          type="twitter"
+          link="https://x.com/The_Crypto_Prof/status/1971361226447098024"
+          description="this is a twitter link explaining how this world works i mean this is just for fun aai "
+        />
+        <Card
+          title="Gold Vs Silved"
+          type="video"
+          link="https://www.youtube.com/watch?v=H58A-Zh4h-U"
+          description="Video on which is a better investement Gold Or Silver"
+        />
+        <Card
+          title="Blog Link"
+          type="link"
+          link="https://personal-blog-tushar.vercel.app/"
+          description="this is my personal blog link "
+        />
+        <Card
+          title="Perosnal notes"
+          type="notes"
+          link=""
+          description="this is my personal note "
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
