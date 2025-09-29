9cf1c15b2bd1016c342da5946ef7fe72a73c94fb
0xtusharganotra <ganotra.vox@gmail.com>
2025-09-29 19:25:52 +0530

    add alpha release badge on ai page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,27 +4,33 @@ import { ShareIcon } from "../icons/ShareIcon";
 import Button from "./button";
 import { useContext } from "react";
 const Header = ({ maintext }: { maintext: string }) => {
-  const { setcreateopen, setshareopen } = useContext(MyContext);
+  const { setcreateopen, setshareopen, bringaipage } = useContext(MyContext);
   return (
     <div
       className="h-[60px] bg-[#0f0f0f] w-[100%] px-10 flex justify-between items-center border-b-1 m-auto border-[#262626]"
       color="white"
     >
       <h2 className="text-xl">{maintext}</h2>
-      <div className="flex gap-3 ">
-        <Button
-          variant="primary"
-          text="Share Memory"
-          startIcon={<PlusIcon />}
-          onClickfn={() => setshareopen(true)}
-        />
-        <Button
-          variant="secondary"
-          text="Add Memory"
-          startIcon={<ShareIcon />}
-          onClickfn={() => setcreateopen(true)}
-        />
-      </div>
+      {bringaipage === false ? (
+        <div className="flex gap-3 ">
+          <Button
+            variant="primary"
+            text="Share Memory"
+            startIcon={<PlusIcon />}
+            onClickfn={() => setshareopen(true)}
+          />
+          <Button
+            variant="secondary"
+            text="Add Memory"
+            startIcon={<ShareIcon />}
+            onClickfn={() => setcreateopen(true)}
+          />
+        </div>
+      ) : (
+        <div className="text-sm bg-[#323232] px-4 py-2 rounded-xl cursor-pointer">
+          Alpha Release
+        </div>
+      )}
     </div>
   );
 };
