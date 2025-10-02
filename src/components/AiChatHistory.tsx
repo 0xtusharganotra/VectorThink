8038774fd7e8a996d1ebf986507f2b90fd8daeb6
0xtusharganotra <ganotra.vox@gmail.com>
2025-10-02 16:08:19 +0530

    feat: create AI chat history component

--- src/components/AiChatHistory.tsx
+++ src/components/AiChatHistory.tsx
@@ -1,5 +1,73 @@
 function AiChatHistory() {
-  return <div></div>;
+  return (
+    <div className="w-[85%] mx-auto mt-10">
+      <div className="flex items-center space-x-2">
+        <p className="text-lg font-semibold text-white whitespace-nowrap">
+          Chat History
+        </p>
+
+        <div className="flex-grow border-t border-gray-500"></div>
+      </div>
+      <div className=" w-[98%] mx-auto py-2 overflow-y-hidden h-[40vh] ">
+        <div className="pt-4 cursor-pointer border-xl hover:font-bold  ">
+          <p className="truncate text-[14px] text-gray-200">
+            Can you please elaborate the functioning of llm{" "}
+          </p>
+          <p className="truncate text-[11px] text-gray-500">
+            Sure! Sir. llm's are large language model that basically converts
+          </p>
+        </div>
+        <div className="pt-4 cursor-pointer border-xl hover:font-bold ">
+          <p className="truncate text-[14px] text-gray-200">
+            Can you please elaborate the functioning of llm{" "}
+          </p>
+          <p className="truncate text-[11px] text-gray-500">
+            Sure! Sir. llm's are large language model that basically converts
+          </p>
+        </div>
+        <div className="pt-4 cursor-pointer border-xl hover:font-bold  ">
+          <p className="truncate text-[14px] text-gray-200">
+            Can you please elaborate the functioning of llm{" "}
+          </p>
+          <p className="truncate text-[11px] text-gray-500">
+            Sure! Sir. llm's are large language model that basically converts
+          </p>
+        </div>
+        <div className="pt-4 cursor-pointer border-xl hover:font-bold  ">
+          <p className="truncate text-[14px] text-gray-200">
+            Can you please elaborate the functioning of llm{" "}
+          </p>
+          <p className="truncate text-[11px] text-gray-500">
+            Sure! Sir. llm's are large language model that basically converts
+          </p>
+        </div>
+        <div className="pt-4 cursor-pointer border-xl hover:font-bold  ">
+          <p className="truncate text-[14px] text-gray-200">
+            Can you please elaborate the functioning of llm{" "}
+          </p>
+          <p className="truncate text-[11px] text-gray-500">
+            Sure! Sir. llm's are large language model that basically converts
+          </p>
+        </div>
+        <div className="pt-4 cursor-pointer border-xl hover:font-bold  ">
+          <p className="truncate text-[14px] text-gray-200">
+            Can you please elaborate the functioning of llm{" "}
+          </p>
+          <p className="truncate text-[11px] text-gray-500">
+            Sure! Sir. llm's are large language model that basically converts
+          </p>
+        </div>
+        <div className="pt-4 cursor-pointer border-xl hover:font-bold  ">
+          <p className="truncate text-[14px] text-gray-200">
+            Can you please elaborate the functioning of llm{" "}
+          </p>
+          <p className="truncate text-[11px] text-gray-500">
+            Sure! Sir. llm's are large language model that basically converts
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AiChatHistory;
