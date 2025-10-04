81d5c287a291db2acd629fa832d53b9d368c7793
0xtusharganotra <ganotra.vox@gmail.com>
2025-10-04 21:39:01 +0530

    feat: add chat history component and allow its interaction with backend to fetch session history of a particular user

--- src/components/AiChatHistory.tsx
+++ src/components/AiChatHistory.tsx
@@ -1,4 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BACKEND_URL } from "../App";
+
+interface sessionsarrprops {
+  userId: string;
+  _id: string;
+  messages: {
+    userMessage: string;
+    aiResponse: string;
+  }[];
+}
+
 function AiChatHistory() {
+  const [sessionarr, setsessionarr] = useState<sessionsarrprops[]>([]);
+
+  useEffect(() => {
+    async function getallusersessions() {
+      const res = await axios.get(`${BACKEND_URL}/v1/ai/chat/all`, {
+        headers: {
+          token: localStorage.getItem("token"),
+        },
+      });
+
+      setsessionarr(res.data.sessions);
+    }
+
+    getallusersessions();
+  }, []);
+
+  function setLocalStorageSessionId(sessionId: string) {
+    localStorage.setItem("ChatSessionId", sessionId);
+    window.location.reload();
+  }
+
   return (
     <div className="w-[85%] mx-auto mt-10">
       <div className="flex items-center space-x-2">
@@ -9,62 +43,19 @@ function AiChatHistory() {
         <div className="flex-grow border-t border-gray-500"></div>
       </div>
       <div className=" w-[98%] mx-auto py-2 overflow-y-hidden h-[40vh] ">
-        <div className="pt-4 cursor-pointer border-xl hover:font-bold  ">
-          <p className="truncate text-[14px] text-gray-200">
-            Can you please elaborate the functioning of llm{" "}
-          </p>
-          <p className="truncate text-[11px] text-gray-500">
-            Sure! Sir. llm's are large language model that basically converts
-          </p>
-        </div>
-        <div className="pt-4 cursor-pointer border-xl hover:font-bold ">
-          <p className="truncate text-[14px] text-gray-200">
-            Can you please elaborate the functioning of llm{" "}
-          </p>
-          <p className="truncate text-[11px] text-gray-500">
-            Sure! Sir. llm's are large language model that basically converts
-          </p>
-        </div>
-        <div className="pt-4 cursor-pointer border-xl hover:font-bold  ">
-          <p className="truncate text-[14px] text-gray-200">
-            Can you please elaborate the functioning of llm{" "}
-          </p>
-          <p className="truncate text-[11px] text-gray-500">
-            Sure! Sir. llm's are large language model that basically converts
-          </p>
-        </div>
-        <div className="pt-4 cursor-pointer border-xl hover:font-bold  ">
-          <p className="truncate text-[14px] text-gray-200">
-            Can you please elaborate the functioning of llm{" "}
-          </p>
-          <p className="truncate text-[11px] text-gray-500">
-            Sure! Sir. llm's are large language model that basically converts
-          </p>
-        </div>
-        <div className="pt-4 cursor-pointer border-xl hover:font-bold  ">
-          <p className="truncate text-[14px] text-gray-200">
-            Can you please elaborate the functioning of llm{" "}
-          </p>
-          <p className="truncate text-[11px] text-gray-500">
-            Sure! Sir. llm's are large language model that basically converts
-          </p>
-        </div>
-        <div className="pt-4 cursor-pointer border-xl hover:font-bold  ">
-          <p className="truncate text-[14px] text-gray-200">
-            Can you please elaborate the functioning of llm{" "}
-          </p>
-          <p className="truncate text-[11px] text-gray-500">
-            Sure! Sir. llm's are large language model that basically converts
-          </p>
-        </div>
-        <div className="pt-4 cursor-pointer border-xl hover:font-bold  ">
-          <p className="truncate text-[14px] text-gray-200">
-            Can you please elaborate the functioning of llm{" "}
-          </p>
-          <p className="truncate text-[11px] text-gray-500">
-            Sure! Sir. llm's are large language model that basically converts
-          </p>
-        </div>
+        {sessionarr.map((session, id) => (
+          <div
+            onClick={() => setLocalStorageSessionId(session._id)}
+            className="pt-4 cursor-pointer border-xl hover:font-bold  "
+          >
+            <p className="truncate text-[14px] text-gray-200">
+              {session?.messages[0]?.userMessage}
+            </p>
+            <p className="truncate text-[11px] text-gray-500">
+              {session?.messages[0]?.aiResponse}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/NewChat.tsx
+++ src/components/NewChat.tsx
@@ -6,13 +6,20 @@ interface NewChatButtonProps {
 }
 
 function NewChatButton(props: NewChatButtonProps) {
-  function popupai() {
-    alert("Ai feature is not live yet");
+  function clearsessionid() {
+    const sessionId = localStorage.getItem("ChatSessionId");
+
+    if (!sessionId) {
+      return;
+    }
+
+    localStorage.removeItem("ChatSessionId");
+    window.location.reload();
   }
   return (
     <div className="mt-4 flex items-center justify-center">
       <button
-        onClick={popupai}
+        onClick={clearsessionid}
         className="w-[90%] mx-auto rounded-xl bg-[#343434] text-white px-4 py-2 shadow-md/20 shadow-gray-200 hover:shadow-none flex justify-center items-center cursor-pointer gap-2"
       >
         <span>{props.Icon}</span> {props.text}
